68abfbb174a7f1d0fe3f79bc9f181a8b5167b8f9
Sharon Cichelli <sharon@invisible-city.com>
2011-10-02 15:05:32 -0500

    running timer, baseline features

---
+++ workouts.js
@@ -0,0 +1,127 @@
+var week1 = [
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 },
+		{ mode: walk, minutes: 1.5 },
+		{ mode: run, minutes: 1 }
+	];
+
+var week2 = [
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 2 },
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 2 },
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 2 },
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 2 },
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 2 },
+	{ mode: run, minutes: 1.5 }
+];
+
+var week3 = [
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 1.5 },
+	{ mode: run, minutes: 3 },
+	{ mode: walk, minutes: 3 },
+	{ mode: run, minutes: 1.5 },
+	{ mode: walk, minutes: 1.5 },
+	{ mode: run, minutes: 3 },
+	{ mode: walk, minutes: 3 },
+	{ mode: run, minutes: 1.5 }
+];
+
+var week4 = [
+	{ mode: run, minutes: 3 },
+	{ mode: walk, minutes: 1.5 },
+	{ mode: run, minutes: 5 },
+	{ mode: walk, minutes: 2.5 },
+	{ mode: run, minutes: 3 },
+	{ mode: walk, minutes: 1.5 },
+	{ mode: run, minutes: 5 }
+];
+
+var C25K = {
+	W1D1 : week1,
+	W1D2 : week1,
+	W1D3 : week1,
+	W2D1: week2,
+	W2D2: week2,
+	W2D3: week2,
+	W3D1: week3,
+	W3D2: week3,
+	W3D3: week3,
+	W4D1: week4,
+	W4D2: week4,
+	W4D3: week4,
+	W5D1 : [
+		{ mode: run, minutes: 5 },
+		{ mode: walk, minutes: 3 },
+		{ mode: run, minutes: 5 },
+		{ mode: walk, minutes: 3 },
+		{ mode: run, minutes: 5 }
+	],
+	W5D2 : [
+		{ mode: run, minutes: 8 },
+		{ mode: walk, minutes: 5 },
+		{ mode: run, minutes: 8 }
+	],
+	W5D3 : [
+		{ mode: run, minutes: 20 }
+	],
+	W6D1 : [
+		{ mode: run, minutes: 5 },
+		{ mode: walk, minutes: 3 },
+		{ mode: run, minutes: 8 },
+		{ mode: walk, minutes: 3 },
+		{ mode: run, minutes: 5 }
+	],
+	W6D2 : [
+		{ mode: run, minutes: 10 },
+		{ mode: walk, minutes: 3 },
+		{ mode: run, minutes: 10 }
+	],
+	W6D3 : [
+		{ mode: run, minutes: 25 }
+	],
+	W7D1 : [
+		{ mode: run, minutes: 25 }
+	],
+	W7D2 : [
+		{ mode: run, minutes: 25 }
+	],
+	W7D3 : [
+		{ mode: run, minutes: 25 }
+	],
+	W8D1 : [
+		{ mode: run, minutes: 28 }
+	],
+	W8D2 : [
+		{ mode: run, minutes: 28 }
+	],
+	W8D3 : [
+		{ mode: run, minutes: 28 }
+	],
+	W9D1 : [
+		{ mode: run, minutes: 30 }
+	],
+	W9D2 : [
+		{ mode: run, minutes: 30 }
+	],
+	W9D3 : [
+		{ mode: run, minutes: 30 }
+	]
+};
